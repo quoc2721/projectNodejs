cb26f3579771dc7cb27c03436daf4383b0481836
Quoc <quoc020721@gmail.com>
2021-12-21 13:15:35 +0700

    login

--- src/Login/login.js
+++ src/Login/login.js
@@ -1,30 +1,7 @@
-import React, { useState } from "react";
+import React from "react";
 import { Container, Typography, TextField, Button } from "@mui/material";
-import { GoogleLogin, GoogleLogout } from "react-google-login";
 
 export default function Login() {
-  const clientId =
-    "28360044120-7fgicc84negovi9o2vmhvhicnnoobh0n.apps.googleusercontent.com";
-
-  const [showloginButton, setShowloginButton] = useState(true);
-  const [showlogoutButton, setShowlogoutButton] = useState(false);
-  const onLoginSuccess = (res) => {
-    console.log("Login Success:", res.profileObj);
-    setShowloginButton(false);
-    setShowlogoutButton(true);
-  };
-
-  const onLoginFailure = (res) => {
-    console.log("Login Failed:", res);
-  };
-
-  const onSignoutSuccess = () => {
-    alert("You have been logged out successfully");
-    console.clear();
-    setShowloginButton(true);
-    setShowlogoutButton(false);
-  };
-
   return (
     <Container
       sx={{
@@ -51,24 +28,6 @@ export default function Login() {
       >
         ĐĂNG NHẬP
       </Button>
-      {showloginButton ? (
-        <GoogleLogin
-          clientId={clientId}
-          buttonText="Sign In"
-          onSuccess={onLoginSuccess}
-          onFailure={onLoginFailure}
-          cookiePolicy={"single_host_origin"}
-          isSignedIn={true}
-        />
-      ) : null}
-
-      {showlogoutButton ? (
-        <GoogleLogout
-          clientId={clientId}
-          buttonText="Sign Out"
-          onLogoutSuccess={onSignoutSuccess}
-        ></GoogleLogout>
-      ) : null}
     </Container>
   );
 }
